68cec373ff514a924c50eb5540eb1ad2df002dcb
AlexanderGrin <sanya10398@gmail.com>
2020-03-12 14:42:55 +0300

    Change styles

--- src/Menu.js
+++ src/Menu.js
@@ -3,24 +3,21 @@ import {BrowserRouter as Router, Link, Route} from "react-router-dom";
 import TrackerForm from "./TrackerForm";
 import TrackersView from "./TrackersView";
 
+import styles from "./style/App.module.scss";
+import classnames from "classnames/bind";
+
+const cx = classnames.bind(styles);
+
 
 
 class Menu extends React.Component{
     render() {
         return (
-            <div>
-                <Router>
+            <div className={cx('Menu')}>
                     <ul id={'nav'}>
                         <li><Link to="/add_tracker">Добавление трекера</Link></li>
                         <li><Link to="/list_trackers">Список трекеров</Link></li>
                     </ul>
-                    {/*<div id={'content'}><Routes/></div>*/}
-                    <div id={'content'}>
-                        <Route exact path={"/add_tracker"} render={()=><TrackerForm updateData={this.props.updateData}/>} />
-                        <Route path={"/list_trackers"} render={()=><TrackersView trackers={this.props.trackers } sortByName={this.props.sortByName}
-                                                                                 sortByPriority={this.props.sortByPriority}/>}/>
-                    </div>
-                </Router>
             </div>
         );
     }
